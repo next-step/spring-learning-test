fec8a326697205578c36aede932f5f0cd660e3d5
boorownie <probitanima11@gmail.com>
2021-01-12 00:30:25 +0900

    feat(auth): sample code

--- src/main/java/nextstep/helloworld/ui/AuthController.java
+++ src/main/java/nextstep/helloworld/ui/AuthController.java
@@ -5,16 +5,22 @@ import nextstep.helloworld.application.AuthorizationException;
 import nextstep.helloworld.dto.MemberResponse;
 import nextstep.helloworld.dto.TokenRequest;
 import nextstep.helloworld.dto.TokenResponse;
+import nextstep.helloworld.infrastructure.AuthorizationExtractor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @RestController
 public class AuthController {
     private static final String SESSION_KEY = "USER";
+    private static final String USERNAME_FIELD = "email";
+    private static final String PASSWORD_FIELD = "password";
     private AuthService authService;
 
 
@@ -22,76 +28,37 @@ public class AuthController {
         this.authService = authService;
     }
 
-    /**
-     * ex) request sample
-     * <p>
-     * POST /login/session HTTP/1.1
-     * content-type: application/x-www-form-urlencoded; charset=ISO-8859-1
-     * host: localhost:55477
-     * <p>
-     * email=email@email.com&password=1234
-     */
     @PostMapping("/login/session")
-    public ResponseEntity sessionLogin() {
-        // TODO: email과 password 값 추출하기
-        String email = "";
-        String password = "";
+    public ResponseEntity sessionLogin(HttpServletRequest request, HttpSession session) {
+        Map<String, String[]> paramMap = request.getParameterMap();
+        String email = paramMap.get(USERNAME_FIELD)[0];
+        String password = paramMap.get(PASSWORD_FIELD)[0];
 
         if (authService.checkInvalidLogin(email, password)) {
             throw new AuthorizationException();
         }
 
-        // TODO: Session에 인증 정보 저장 (key: SESSION_KEY, value: email값)
+        session.setAttribute(SESSION_KEY, email);
 
         return ResponseEntity.ok().build();
     }
 
-    /**
-     * ex) request sample
-     * <p>
-     * GET /members/me HTTP/1.1
-     * cookie: JSESSIONID=E7263AC9557EF658C888F02EEF840A19
-     * accept: application/json
-     */
     @GetMapping("/members/me")
-    public ResponseEntity findMyInfo() {
-        // TODO: Session을 통해 인증 정보 조회하기 (key: SESSION_KEY)
-        String email = "";
+    public ResponseEntity findMyInfo(HttpSession session) {
+        String email = (String) session.getAttribute(SESSION_KEY);
         MemberResponse member = authService.findMember(email);
         return ResponseEntity.ok().body(member);
     }
 
-    /**
-     * ex) request sample
-     * <p>
-     * POST /login/token HTTP/1.1
-     * accept: application/json
-     * content-type: application/json; charset=UTF-8
-     * <p>
-     * {
-     * "email": "email@email.com",
-     * "password": "1234"
-     * }
-     */
     @PostMapping("/login/token")
-    public ResponseEntity tokenLogin() {
-        // TODO: TokenRequest 값을 메서드 파라미터로 받아오기 (hint: @RequestBody)
-        TokenRequest tokenRequest = null;
+    public ResponseEntity tokenLogin(@RequestBody TokenRequest tokenRequest) {
         TokenResponse tokenResponse = authService.createToken(tokenRequest);
         return ResponseEntity.ok().body(tokenResponse);
     }
 
-    /**
-     * ex) request sample
-     * <p>
-     * GET /members/you HTTP/1.1
-     * authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbWFpbEBlbWFpbC5jb20iLCJpYXQiOjE2MTAzNzY2NzIsImV4cCI6MTYxMDM4MDI3Mn0.Gy4g5RwK1Nr7bKT1TOFS4Da6wxWh8l97gmMQDgF8c1E
-     * accept: application/json
-     */
     @GetMapping("/members/you")
     public ResponseEntity findYourInfo(HttpServletRequest request) {
-        // TODO: authorization 헤더의 Bearer 값을 추출하기
-        String token = "";
+        String token = AuthorizationExtractor.extract(request);
         MemberResponse member = authService.findMemberByToken(token);
         return ResponseEntity.ok().body(member);
     }
