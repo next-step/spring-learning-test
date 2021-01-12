6eeeab0ca177d9888f0e362ae1dbd925b47715dc
boorownie <probitanima11@gmail.com>
2021-01-12 12:33:44 +0900

    feat(jdbc): default code

---
+++ src/main/java/nextstep/helloworld/jdbc/jdbctemplate/QueryingDAO.java
@@ -0,0 +1,66 @@
+package nextstep.helloworld.jdbc.jdbctemplate;
+
+import nextstep.helloworld.jdbc.Customer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class QueryingDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    public QueryingDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final RowMapper<Customer> actorRowMapper = (resultSet, rowNum) -> {
+        Customer customer = new Customer(
+                resultSet.getLong("id"),
+                resultSet.getString("first_name"),
+                resultSet.getString("last_name")
+        );
+        return customer;
+    };
+
+    /**
+     * public <T> T queryForObject(String sql, Class<T> requiredType)
+     */
+    public int count() {
+        String sql = "select count(*) from customers";
+        return 0;
+    }
+
+    /**
+     * public <T> T queryForObject(String sql, Class<T> requiredType, @Nullable Object... args)
+     */
+    public String getLastName(Long id) {
+        String sql = "select last_name from customers where id = ?";
+        return null;
+    }
+
+    /**
+     * public <T> T queryForObject(String sql, RowMapper<T> rowMapper, @Nullable Object... args)
+     */
+    public Customer findCustomerById(Long id) {
+        String sql = "select id, first_name, last_name from customers where id = ?";
+        return null;
+    }
+
+    /**
+     * public <T> List<T> query(String sql, RowMapper<T> rowMapper)
+     */
+    public List<Customer> findAllCustomers() {
+        String sql = "select id, first_name, last_name from customers";
+        return null;
+    }
+
+    /**
+     * public <T> List<T> query(String sql, RowMapper<T> rowMapper, @Nullable Object... args)
+     */
+    public List<Customer> findCustomerByFirstName(String firstName) {
+        String sql = "select id, first_name, last_name from customers where first_name = ?";
+        return null;
+    }
+}
