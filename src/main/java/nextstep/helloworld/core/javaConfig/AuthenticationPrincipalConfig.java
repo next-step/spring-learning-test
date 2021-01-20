0a5a12a7f9a3bdf4b046d08430b6189cbdccb00e
boorownie <probitanima11@gmail.com>
2021-01-20 23:24:18 +0900

    feat: sample code
    Signed-off-by: boorownie <probitanima11@gmail.com>

--- src/main/java/nextstep/helloworld/core/javaConfig/AuthenticationPrincipalConfig.java
+++ src/main/java/nextstep/helloworld/core/javaConfig/AuthenticationPrincipalConfig.java
@@ -1,15 +1,21 @@
 package nextstep.helloworld.core.javaConfig;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 // TODO: Java-based Configuration을 하기 위한 클래스로 지정하기
+@Configuration
 public class AuthenticationPrincipalConfig {
 
     // TODO: AuthService 빈을 등록하기
+    @Bean
     public AuthService authService() {
-        return null;
+        return new AuthService();
     }
 
     // TODO: AuthenticationPrincipalArgumentResolver를 빈 등록하고 authService에 대한 의존성을 주입하기
+    @Bean
     public AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver() {
-        return null;
+        return new AuthenticationPrincipalArgumentResolver(authService());
     }
 }
