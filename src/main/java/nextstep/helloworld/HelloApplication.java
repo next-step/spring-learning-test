1ea6723833bddfc31ad6c4e82c6c6260db55c311
yeeun-song <yeeun.song@toss.im>
2023-04-22 21:50:34 +0900

    yeny

--- src/main/java/nextstep/helloworld/HelloApplication.java
+++ src/main/java/nextstep/helloworld/HelloApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloApplication {
     public static void main(String[] args) {
+        // yeny
         SpringApplication.run(HelloApplication.class, args);
     }
 }
