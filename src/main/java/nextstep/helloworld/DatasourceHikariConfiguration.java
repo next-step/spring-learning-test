a241c40d69d4a9a62514d7effd9565f99d573f74
boorownie <probitanima11@gmail.com>
2023-05-08 18:01:13 +0900

    sample

--- src/main/java/nextstep/helloworld/DatasourceHikariConfiguration.java
+++ src/main/java/nextstep/helloworld/DatasourceHikariConfiguration.java
@@ -14,6 +14,8 @@ public class DatasourceHikariConfiguration {
     @Bean
     public DataSource getDataSource() {
         HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:test");
+        hikariConfig.setMaximumPoolSize(20);
 
         return new HikariDataSource(hikariConfig);
     }
--- src/main/java/nextstep/helloworld/DatasourceJavaConfiguration.java
+++ src/main/java/nextstep/helloworld/DatasourceJavaConfiguration.java
@@ -13,6 +13,10 @@ public class DatasourceJavaConfiguration {
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:test");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
         return dataSourceBuilder.build();
     }
 }
